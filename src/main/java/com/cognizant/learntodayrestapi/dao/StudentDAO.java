de916e14a4ac08ab8e88dc3082f5f95d64fd73c4
Shivani <shivanipalley98@gmail.com>
2021-03-24 18:32:37 +0530

    Student Adding to the DB done

--- src/main/java/com/cognizant/learntodayrestapi/dao/StudentDAO.java
+++ src/main/java/com/cognizant/learntodayrestapi/dao/StudentDAO.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
+import com.cognizant.learntodayrestapi.exception.StudentNotFoundException;
 import com.cognizant.learntodayrestapi.model.Student;
 
+import java.rmi.StubNotFoundException;
 import java.util.*;
 
 @Component
@@ -15,10 +17,26 @@ public class StudentDAO {
 	private JdbcTemplate jdbcTemplate;
 
 	public boolean saveStudent(Student s) {
-		return false;
+
+		boolean isAdded = false;
+
+		try {
+			int noOfRowsUpdated = jdbcTemplate.update("insert into student values (?,?,?)", s.getEnrollmentId(),
+					s.getStudentId(), s.getCourseId());
+
+			if (noOfRowsUpdated == 1) {
+				isAdded = true;
+			}
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+
+		return isAdded;
 	}
 
 	public boolean deleteStudent(int id) {
+		
+		
 		return false;
 	}
 }
