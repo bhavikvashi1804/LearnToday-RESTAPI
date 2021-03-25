075573b406d6a7853ce62c102a024139021201ae
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2021-03-25 10:11:14 +0530

    Admin Controller working done

---
+++ src/main/java/com/cognizant/learntodayrestapi/service/CourseService.java
@@ -0,0 +1,30 @@
+package com.cognizant.learntodayrestapi.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cognizant.learntodayrestapi.dao.CourseDAO;
+import com.cognizant.learntodayrestapi.exception.CourseNotFoundException;
+import com.cognizant.learntodayrestapi.model.Course;
+
+@Service
+public class CourseService {
+
+	@Autowired
+	private CourseDAO courseDAO;
+
+	public List<Course> getAllCourses() {
+		return courseDAO.getAllCourses();
+	}
+
+	public List<Course> getAllCoursesSortByStartDate() {
+		return courseDAO.getAllCoursesSortByStartDate();
+	}
+
+	public Course getCourseById(int courseId) throws CourseNotFoundException {
+		return courseDAO.getCourseById(courseId);
+	}
+
+}
